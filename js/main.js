06052281b9eee2c57e40df1462294127c8bd301f
jusrus01 <jjustinas.ruslys@gmail.com>
2021-07-31 20:23:41 +0300

    Update

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
+const menuButton = document.getElementById("menuButton");
+const header = document.getElementById("header");
+const body = document.querySelector("html");
+const menu = document.querySelector(".header--menu");
+
+menuButton.addEventListener("click", showMenuItems);
+menu.addEventListener("click", showMenuItems);
+
+function showMenuItems() {
+    if(header.classList.contains("open")) {
+        header.classList.remove("open");
+        body.classList.remove("disable-scroll");
+    } else {
+        header.classList.add("open");
+        body.classList.add("disable-scroll");
+    }
+}
 
