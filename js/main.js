dbc374a0a335b77df5c411bf96aec55a7743b84c
jusrus01 <jjustinas.ruslys@gmail.com>
2021-08-03 14:43:32 +0300

    .

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ const menuButton = document.getElementById("menuButton");
 const header = document.getElementById("header");
 const body = document.querySelector("html");
 const menu = document.querySelector(".header--menu");
+const overlay = document.querySelector(".overlay");
 
 menuButton.addEventListener("click", showMenuItems);
 menu.addEventListener("click", showMenuItems);
@@ -10,9 +11,26 @@ function showMenuItems() {
     if(header.classList.contains("open")) {
         header.classList.remove("open");
         body.classList.remove("disable-scroll");
+
+        if(overlay.classList.contains("fade-in")) {
+            overlay.classList.remove("fade-in");
+            menu.classList.remove("fade-in");
+        }
+
+        overlay.classList.add("fade-out");
+        menu.classList.add("fade-out");
+
     } else {
         header.classList.add("open");
         body.classList.add("disable-scroll");
+
+        if(overlay.classList.contains("fade-out")) {
+            overlay.classList.remove("fade-out");
+            menu.classList.remove("fade-out");
+        }
+
+        overlay.classList.add("fade-in");
+        menu.classList.add("fade-in");
     }
 }
 
